4669f27f291c85a9ee33b9c8503170cd557023d2
Noah <noahkoleske@icloud.com>
2023-07-06 22:30:52 -0500

    added click event for save btn

--- assets/script.js
+++ assets/script.js
@@ -8,19 +8,12 @@ var time = dayjs().format('h:m')
 console.log(time);
 
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  var textSave = $('.description').val()
-  var hourX = $('.text-center').val()
 
   $('.saveBtn').on('click', function () {
-
-    localStorage.setItem(hourX, textSave)
+    var saveTime = $(this).parent().attr("id").split("-")[1]
+    var message = $(this).siblings(".description").val()
+    localStorage.setItem(saveTime, message)
+    
   })
 
   // TODO: Add code to apply the past, present, or future class to each time

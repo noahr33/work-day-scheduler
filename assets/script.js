e403530b7b38365d5d6c2c3c27bd1e0f4b74a4cf
Noah <noahkoleske@icloud.com>
2023-07-08 19:14:46 -0500

    added time color function

--- assets/script.js
+++ assets/script.js
@@ -2,30 +2,48 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-var now = dayjs().format('dddd, MMMM D')
-console.log(now);
-var time = dayjs().format('h:m')
-console.log(time);
 
 $(function () {
-
+  var now = dayjs().format('dddd, MMMM D')
+  var time = dayjs().format('h:m')
+  var timeBlock = $('.time-block')
+  
+  
+  // Saves message upon clicking save button and stores it locally
   $('.saveBtn').on('click', function () {
-    var saveTime = $(this).parent().attr("id").split("-")[1]
+    var saveTime = parseInt($(this).parent().attr("id").split("-")[1])
     var message = $(this).siblings(".description").val()
+    console.log(saveTime);
     localStorage.setItem(saveTime, message)
+  })
+
+  // Adds corresponding class to each time block to represent past, present, and future
+  timeBlock.each(function () {
+    var timeBlock = $(this)
+    var blockHour = parseInt(timeBlock.attr('id').split("-")[1])
+    var currentTime = parseInt(dayjs().hour())
+
+    console.log(blockHour, currentTime);
+    
+    var timeClass
+    if (blockHour === currentTime) {
+      timeClass = 'present'
+    } else if (blockHour < currentTime) {
+      timeClass = 'past'
+    } else {
+      timeClass = 'future'
+    }
+
+    timeBlock.addClass(timeClass)
     
   })
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+
+  
+  // Shows current date in header
   $('#currentDay').append(now)
 });
